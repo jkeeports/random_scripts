78ee10b4cd2629b500892f92117b48ddbbdc98d3
jkeeports <17264690+jkeeports@users.noreply.github.com>
2021-12-28 10:26:37 -0500

    Create WordUnlocker.ps1

---
+++ WordUnlocker.ps1
@@ -0,0 +1,67 @@
+#unlocks password protected word documents so they can be edited freely, just in case one needs to be edited. 
+#Created 6-19-2013  https://github.com/dasu/powershell-scripts/blob/master/Word%20unlocker.ps1
+#Modified 12-28-2021
+
+if (!(Test-Path -path C:\TEMP\UNLOCK\)) {New-Item C:\TEMP\UNLOCK -Type Directory}
+Remove-Item -recurse C:\TEMP\UNLOCK\*
+
+# function Select-FileDialog
+# {
+# 	param([string]$Title,[string]$Directory,[string]$Filter="All Files (*.*)|*.*")
+# 	[System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms") | Out-Null
+# 	$objForm = New-Object System.Windows.Forms.OpenFileDialog
+# 	$objForm.InitialDirectory = $Directory
+# 	$objForm.Filter = $Filter
+# 	$objForm.Title = $Title
+# 	$Show = $objForm.ShowDialog()
+# 	If ($Show -eq "OK")
+# 	{
+# 		Return $objForm.FileName
+# 	}
+# 	Else
+# 	{
+# 		Write-Error "Operation cancelled by user."
+# 	}
+# }
+
+#$FILE = Select-FileDialog -Title "Select a locked word document" -Filter "Word Documents|*.docx"
+
+# SET YOUR PATH ON THE NEXT LINE
+foreach ($FILE in Get-ChildItem "C:\Users\YOURUSER\Documents\" -Include *.doc)
+    $DEST = Copy-Item $FILE -destination C:\TEMP\ -PassThru
+    $FNAME = $DEST.Name
+    $NZIP = $FNAME -replace ".docx",".zip"
+    $ZIP = Rename-Item $DEST -NewName $NZIP -PassThru
+    $shell_app=new-object -com shell.application
+    $zip_file = $shell_app.namespace(($ZIP).FullName)
+    $destination = $shell_app.namespace("C:\TEMP\UNLOCK")
+    $destination.Copyhere($zip_file.items())
+    Remove-Item $ZIP.FullName
+    set-content $ZIP.FullName ("PK" + [char]5 + [char]6 + ("$([char]0)" * 18))
+
+    $xml = New-Object xml
+    $xml.Load("C:\TEMP\UNLOCK\word\settings.xml")
+    $XML.settings.documentProtection|foreach {$_.Enforcement = '0'}
+    $xml.save("C:\TEMP\UNLOCK\word\settings.xml")
+
+    $zip_file.CopyHere("C:\TEMP\UNLOCK\Customxml")
+    sleep 1
+    $zip_file.CopyHere("C:\TEMP\UNLOCK\docProps")
+    sleep 1
+    $zip_file.CopyHere("C:\TEMP\UNLOCK\word")
+    sleep 2
+    $zip_file.CopyHere("C:\TEMP\UNLOCK\_rels")
+    sleep 1
+    $zip_file.CopyHere("C:\TEMP\UNLOCK\[Content_Types].XML")
+    sleep 1
+    $UDOCX = $ZIP.Name -replace ".zip","_UNLOCKED.docx"
+    $UFILE = Rename-Item $ZIP.FullName -newname $UDOCX -PassThru
+    Move-Item $UFILE.FullName -Destination (split-path $FILE)
+    Remove-Item -recurse C:\TEMP\UNLOCK\*
+
+    echo $FILE
+    echo $DEST
+    echo $FNAME
+    echo $NZIP
+    echo $ZIP.FullName
+}
